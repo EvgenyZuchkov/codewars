d10d4ae67daf6002bb7ded07707d5f756e3a6d91
Evgeny Zuchkov <jenyazuchkov@gmail.com>
2026-06-03 20:00:45 +0300

    Calculate average

--- 8kuy/8kuy/Calculate average.js
+++ 8kuy/8kuy/Calculate average.js
@@ -1,11 +1,13 @@
 function findAverage(array) {
-    if (array.length !== 0) {
-        let sum = 0;
-        array.forEach((item) => {
-            sum += item
-        })
-        return sum / array.length;
-    } else {
-        return 0;
-    }
+    // if (array.length !== 0) {
+    //     let sum = 0;
+    //     array.forEach((item) => {
+    //         sum += item
+    //     })
+    //     return sum / array.length;
+    // } else {
+    //     return 0;
+    // }
+
+    return array.length === 0 ? 0 : array.reduce((acc, num) => acc + num, 0)/array.length;
 }
